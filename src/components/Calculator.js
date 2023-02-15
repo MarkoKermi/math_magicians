72625d6c765fb35cdbe2713a3b2fddb53b9faa8d
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-15 20:34:28 +0100

    events setup

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,84 @@
-import './Calculator.css';
+import React, { useState } from "react";
+import "./Calculator.css";
+// import calculate from "./logic/calculate";
+import calculate from "./logic/calculate";
 
 function Calculator() {
+  const [result, setResult] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setResult(calculate(result, buttonName));
+  };
+
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="screen" />
-        <div className="btn light-gray">AC</div>
-        <div className="btn light-gray">%</div>
-        <div className="btn light-gray">+/-</div>
-        <div className="btn orange">/</div>
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
-        <div className="btn orange">X</div>
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
-        <div className="btn orange">+</div>
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
-        <div className="btn orange">-</div>
-        <div className="btn">0</div>
-        <div className="btn">.</div>
-        <div className="btn">=</div>
+        <div
+          className="screen"
+          type="text"
+          value={result.next || result.total || "0"}
+        />
+        <div className="btn light-gray" onClick={() => handleClick("AC")}>
+          AC
+        </div>
+        <div className="btn light-gray" onClick={() => handleClick("%")}>
+          %
+        </div>
+        <div className="btn light-gray" onClick={() => handleClick("+/-")}>
+          +/-
+        </div>
+        <div className="btn orange" onClick={() => handleClick("/")}>
+          /
+        </div>
+        <div className="btn" onClick={() => handleClick("7")}>
+          7
+        </div>
+        <div className="btn" onClick={() => handleClick("8")}>
+          8
+        </div>
+        <div className="btn" onClick={() => handleClick("9")}>
+          9
+        </div>
+        <div className="btn orange" onClick={() => handleClick("X")}>
+          X
+        </div>
+        <div className="btn" onClick={() => handleClick("4")}>
+          4
+        </div>
+        <div className="btn" onClick={() => handleClick("5")}>
+          5
+        </div>
+        <div className="btn" onClick={() => handleClick("6")}>
+          6
+        </div>
+        <div className="btn orange" onClick={() => handleClick("+")}>
+          +
+        </div>
+        <div className="btn" onClick={() => handleClick("1")}>
+          1
+        </div>
+        <div className="btn" onClick={() => handleClick("2")}>
+          2
+        </div>
+        <div className="btn" onClick={() => handleClick("3")}>
+          3
+        </div>
+        <div className="btn orange" onClick={() => handleClick("-")}>
+          -
+        </div>
+        <div className="btn" onClick={() => handleClick("0")}>
+          0
+        </div>
+        <div className="btn" onClick={() => handleClick(".")}>
+          .
+        </div>
+        <div className="btn" onClick={() => handleClick("=")}>
+          =
+        </div>
       </div>
     </div>
   );

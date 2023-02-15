194c6810dcf1b67a78c8ec261ce03293035a4263
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-15 10:25:07 +0100

    setup linters errors

--- src/App.js
+++ src/App.js
@@ -1,86 +1,9 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import Calculator from './components/Calculator';
 
 function App() {
-  const [preState, setPreState] = useState("");
-  const [curState, setCurState] = useState("");
-  const [input, setInput] = useState("0");
-  const [operator, setOperator] = useState(null);
-  const [total, setTotal] = useState(false);
-
-  const inputNum = (e) => {};
-
-  const operatorType = (e) => {};
-
-  const equals = (e) => {};
-
-  const minusPlus = (e) => {};
-
-  const percent = (e) => {};
-
-  const reset = (e) => {};
   return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="screen" />
-        <div className="btn light-gray" onClick={reset}>
-          AC
-        </div>
-        <div className="btn light-gray" onClick={percent}>
-          %
-        </div>
-        <div className="btn light-gray" onClick={minusPlus}>
-          +/-
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          /
-        </div>
-        <div className="btn" onClick={inputNum}>
-          7
-        </div>
-        <div className="btn" onClick={inputNum}>
-          8
-        </div>
-        <div className="btn" onClick={inputNum}>
-          9
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          X
-        </div>
-        <div className="btn" onClick={inputNum}>
-          4
-        </div>
-        <div className="btn" onClick={inputNum}>
-          5
-        </div>
-        <div className="btn" onClick={inputNum}>
-          6
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          +
-        </div>
-        <div className="btn" onClick={inputNum}>
-          1
-        </div>
-        <div className="btn" onClick={inputNum}>
-          2
-        </div>
-        <div className="btn" onClick={inputNum}>
-          3
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          -
-        </div>
-        <div className="btn" onClick={inputNum}>
-          0
-        </div>
-        <div className="btn" onClick={inputNum}>
-          .
-        </div>
-        <div className="btn" onClick={equals}>
-          =
-        </div>
-      </div>
+    <div className="App">
+      <Calculator />
     </div>
   );
 }

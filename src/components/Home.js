744981f7e5a4d87b3a3078ccd8730ed0da3db123
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-22 10:57:35 +0100

    setup linters error based on first review

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,3 @@
-/* eslint-disable max-len */
-// import design from '../design/Home.css';
-// eslint-disable-next-line import/extensions
 import design from './design/Home.css';
 
 const Home = () => (
@@ -8,10 +5,14 @@ const Home = () => (
     <h2 className={design.homeTitle}>Welcome to my Math Magicians website!</h2>
     <div className={design.homeParagraphs}>
       <p className={design.paragraphOne}>
-        I would not dare to say that there is a direct relation between mathematics and madness, but there is no doubt that great mathematicians suffer from maniacal characteristics, delirium and symptoms of schizophrenia.
+        I would not dare to say that there is a direct relation between mathematics and madness;
+        but there is no doubt that great mathematicians suffer from maniacal characteristics,
+        delirium and symptoms of schizophrenia.
       </p>
       <p className={design.paragraphTwo}>
-        Why are numbers beautiful? It’s like asking why is Beethoven’s Ninth Symphony beautiful. If you don’t see why, someone can’t tell you. I know numbers are beautiful. If they aren’t beautiful, nothing is.
+        Why are numbers beautiful? It is like asking why is Beethoven’s Ninth Symphony beautiful
+        If you don’t see why, someone can’t tell you.
+        I know numbers are beautiful. If they aren’t beautiful, nothing is.
       </p>
     </div>
   </div>

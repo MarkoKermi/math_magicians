df1903dc8e37c799bbca80ac4b5129098cc4639d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-23 12:39:42 +0100

    add test for calculator components

---
+++ src/test/snapshot.test.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import Home from '../components/home';
+import Quote from '../components/quote';
+import Calculator from '../components/Calculator';
+
+describe('Snapshot Test for Math Magicians project', () => {
+  it('Snapshot needs to be eaqual as component render', () => {
+    const viewHome = render(<Home />);
+    expect(viewHome).toMatchSnapshot();
+  });
+  it('Snapshot needs to be eaqual as component render', () => {
+    const viewQuote = render(<Quote />);
+    expect(viewQuote).toMatchSnapshot();
+  });
+  it('Snapshot needs to be eaqual as component render', () => {
+    const calculatorView = render(<Calculator />);
+    expect(calculatorView).toMatchSnapshot();
+  });
+});

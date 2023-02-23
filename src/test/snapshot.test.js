b31cb7aa1e411532c1051dc5167e58bd56e21b1d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-23 13:35:23 +0100

    fix path error

--- src/test/snapshot.test.js
+++ src/test/snapshot.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Home from '../components/home';
+import Home from '../components/Home';
 import Quote from '../components/quote';
 import Calculator from '../components/Calculator';
 
